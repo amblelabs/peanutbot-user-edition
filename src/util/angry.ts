13c78ebb41cbfb5ac2b103dd5bdc8a7b613ee0d7
vmbbi (Max) <yuhimcheng2@gmail.com>
2026-09-15 00:44:32 +0800

    idk

--- src/util/angry.ts
+++ src/util/angry.ts
@@ -6,12 +6,7 @@ const url =
   "http://raw.githubusercontent.com/amblelabs/peanutbot/master/assets/angry.png";
 
 async function sendAngry(message: Message) {
-    await cache.uncache(url, (options) =>
-        message.reply({
-            content: config.fun.wrath.message,
-            files: options.files ?? (options.content ? [options.content] : []),
-        })
-    );
+    await cache.uncache(url, (m) => message.reply(m));
 }
 export default {
   sendAngry,
